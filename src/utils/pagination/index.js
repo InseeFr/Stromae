f75a372776e5bc355192a357b432e566130dc5b3
Laurent Caouissin <laurent.caouissin@insee.fr>
2021-02-23 16:02:15 +0100

    Create utils functions for pagination

--- src/utils/pagination/index.js
+++ src/utils/pagination/index.js
@@ -7,3 +7,25 @@ const pageInLoop = { iteration: 5, page: 6 };
 // 3eme composant du questionnaire, 2ème itérations, page 5, 4ème itérations, 7ème composants
 const pageInLoopInLoopInQuestionnaire = [3, 2, 5, 4, 7];
 const v2 = { page: 3 };
+
+const sequencePagination = 'sequence';
+const subsequencePagination = 'subsequence';
+// const questionPagination = 'question';
+
+export const getListOfPages = pagination => components => {
+  if (pagination === sequencePagination) {
+    return components.filter(
+      ({ componentType }) =>
+        componentType === 'Sequence' || componentType === 'Loop'
+    );
+  }
+  if (pagination === subsequencePagination) {
+    return components.filter(
+      ({ componentType }) => componentType === 'Subsequence'
+    );
+  }
+  return components;
+};
+
+export const getComponentsOfSequence = components => sequenceId =>
+  components.filter(({ idSequence }) => idSequence === sequenceId);
