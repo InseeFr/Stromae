30bc845df34dd4ccf8c087e2325a3026c39c6eed
Matthew Clark <matthew.clark@insee.fr>
2023-02-02 15:27:05 +0100

    adding startReactDsfr

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { startReactDsfr } from "@codegouvfr/react-dsfr/spa";
+startReactDsfr({ defaultColorScheme: "system" });
+
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
