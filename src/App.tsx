90e24fe6aa64a5b7a4f162656dabc1b7174b73b1
Matt Clark <87824367+isMattCoding@users.noreply.github.com>
2023-02-02 11:20:57 +0100

    Update App.tsx
    removing un merged conflict resolution

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-<<<<<<< HEAD
 import WelcomeSurvey from "./pages/WelcomeSurvey";
-=======
-import * as lunatic from "@inseefr/lunatic";
->>>>>>> 0ada76658751ed6b705602751e5610ec218218a6
 import Welcome from "./pages/welcome";
 import ErrorPage from "./pages/error";
 
