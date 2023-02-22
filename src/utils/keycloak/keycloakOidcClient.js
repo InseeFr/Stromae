3cb338e8899ac14990750ce6dd2360d6b97d5e94
Laurent Caouissin <caouissin.laurent@gmail.com>
2023-02-22 14:16:19 +0100

    fix: pathname to activate readonly auth mode

--- src/utils/keycloak/keycloakOidcClient.js
+++ src/utils/keycloak/keycloakOidcClient.js
@@ -1,4 +1,5 @@
 import Keycloak from 'keycloak-js';
+import { READ_ONLY } from 'utils/constants';
 
 const getCurrentSurvey = (path) => {
   const temp = path.split('/questionnaire/');
@@ -31,7 +32,7 @@ export const createKeycloakOidcClient = async ({
     await keycloakInstance.login({
       redirectUri: window.location.href,
       // Readonly mode : Internal user login
-      idpHint: window.location.pathname.startsWith(`/404`)
+      idpHint: window.location.pathname.startsWith(`/${READ_ONLY}`)
         ? identityProvider
         : null,
     });
