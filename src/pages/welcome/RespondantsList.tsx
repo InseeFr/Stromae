2a08c4899588a23d833b2b7bd9bd416c8fdef50b
Julia Dirand <julia.dirand@insee.fr>
2023-03-13 13:00:36 +0100

    add questions

--- src/pages/welcome/RespondantsList.tsx
+++ src/pages/welcome/RespondantsList.tsx
@@ -1,7 +1,7 @@
-export function RespondantsList(metadata: any) {
-    const respondants = metadata.Welcome.whoAnswers;
-    if (respondants.length > 0) {
-        const listRespondants = respondants.map((respondant: String) => <li>{respondant}</li>);
+export function RespondantsList(props: { respondants: string[] }) {
+    const respondants = props.respondants;
+    if (respondants && respondants.length > 0) {
+        const listRespondants = respondants.map((respondant: string, index: number) => <li key={index}>{respondant}</li>);
         return (
             <div className="fr-col-12">
                 <h2>Qui doit répondre à ce questionnaire?</h2>
--- src/pages/welcome/WelcomeContainer.tsx
+++ src/pages/welcome/WelcomeContainer.tsx
@@ -30,8 +30,8 @@ export function WelcomeContainer() {
             <div className="fr-col-12">
                 <p className="fr-text--lead">{metadata.Welcome.Enq_ObjectifsCourts}</ p>
             </div>
-            <RespondantsList metadata={metadata} />
-            <div className="fr-col-12">
+            <RespondantsList respondants={metadata.Welcome.whoAnswers} />
+            <div className="fr-col-12 fr-mt-2w">
                 {/* To do: rediriger vers la première question */}
                 <Button
                     size="large"
@@ -42,10 +42,30 @@ export function WelcomeContainer() {
                     Commencer
                 </Button>
             </div>
+            <div className="fr-col-12 fr-mt-5w">
+                <h3>En savoir plus sur l'{metadata.Welcome.Enq_LibelleEnquete}</ h3>
+            </div>
             <div className="fr-col-12">
                 <div className={fr.cx('fr-accordions-group')}>
-                    <Accordion label="Quel est le cadre légal de l'enquête?">Content of the Accordion 1</Accordion>
-                    <Accordion label="Name of the Accordion 2">Content of the Accordion 2</Accordion>
+                    <Accordion label="Quel est le cadre légal de l'enquête?">
+                        <p>
+                            Vu l'avis favorable du Conseil national de l'information statistique, cette enquête
+                            {metadata.Welcome.Enq_CaractereObligatoire && metadata.Welcome.Enq_QualiteStatistique && ', reconnue d’intérêt général et de qualité statistique'}
+                            {metadata.Welcome.Enq_CaractereObligatoire && !metadata.Welcome.Enq_QualiteStatistique && ' reconnue d’intérêt général'}
+                            {metadata.Welcome.Enq_QualiteStatistique && !metadata.Welcome.Enq_CaractereObligatoire && ' de qualité statistique'}
+                            {metadata.Welcome.Enq_CaractereObligatoire ? ' est obligatoire' : " est reconnue d'intérêt général"}, en application de la loi n° 51-711 du 7 juin 1951 sur l'obligation, la coordination et le secret en matière de statistiques.
+                        </p>
+                        <p>
+                            Visa n°{metadata.Welcome.Enq_NumeroVisa} du Ministre {metadata.Welcome.Enq_MinistereTutelle}, valable pour l'année {metadata.Welcome.Enq_AnneeVisa} - Arrêté en {metadata.Welcome.Enq_DateParutionJo ? `en date du ${metadata.Welcome.Enq_DateParutionJo}` : "cours de parution"}.
+                        </p>
+                        <p>
+                            Les réponses à ce questionnaire sont protégées par le secret statistique et destinées à {metadata.Welcome.Enq_RespOperationnel}.
+                            Le règlement général 2016/679 du 27 avril 2016 sur la protection des données (RGPD) ainsi que la loi n° 78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés, s'appliquent à la présente enquête. Ils garantissent aux personnes concernées un droit d'accès, de limitation ou de rectification pour les données les concernant. Ce droit peut être exercé auprès de {metadata.Welcome.Enq_RespTraitement}.
+                        </p>
+                    </Accordion>
+                    {metadata.Welcome.Enq_QuestionsAdditionnelles.map((questionAdditionnelle: { question: string, contenu: string }, index: number) =>
+                        <Accordion label={questionAdditionnelle.question} key={index}>{questionAdditionnelle.contenu}</Accordion>
+                    )}
                 </div>
             </div>
         </div >
