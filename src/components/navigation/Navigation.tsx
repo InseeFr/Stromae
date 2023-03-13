97c1185f907a10caea6023bb3c373cb9835d264d
Renaud Genevois <renaud.genevois@insee.fr>
2023-03-13 15:23:34 +0100

    just a button

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,35 +1,18 @@
-import { ButtonsGroup } from '@codegouvfr/react-dsfr/ButtonsGroup';
+import { Button } from '@codegouvfr/react-dsfr/Button';
 import { OrchestratedElement } from '../orchestrator';
 
 export function Navigation(props: OrchestratedElement) {
-	const {
-		goNextPage = () => null,
-		goPreviousPage,
-		isFirstPage,
-		isLastPage,
-	} = props;
+	const { goNextPage = () => null, isLastPage } = props;
 
 	function handleClick() {
 		goNextPage();
 	}
-
-	return (
-		<ButtonsGroup
-			buttons={[
-				{
-					children: 'Page précédente',
-					iconId: 'fr-icon-arrow-left-line',
-					onClick: goPreviousPage,
-					disabled: isFirstPage,
-				},
-				{
-					children: 'Page Suivante',
-					iconId: 'fr-icon-arrow-right-line',
-					onClick: handleClick,
-					disabled: isLastPage,
-				},
-			]}
-			inlineLayoutWhen="always"
-		/>
-	);
+	if (!isLastPage) {
+		return (
+			<Button priority="primary" onClick={handleClick}>
+				Continuer
+			</Button>
+		);
+	}
+	return null;
 }
