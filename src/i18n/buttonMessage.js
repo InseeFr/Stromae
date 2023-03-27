49c7f2177f0ab26721a43591bf57039a16f2f1dc
MickaelMenet <mickaelmenet@gmail.com>
2023-03-27 16:55:09 +0200

    fix: add new json & modify continue button

--- src/i18n/buttonMessage.js
+++ src/i18n/buttonMessage.js
@@ -1,8 +1,8 @@
 const buttonMessage = {
   start: { fr: `Commencer`, en: `Start` },
   saveAndNext: {
-    fr: `Enregistrer et continuer`,
-    en: `Save and continue`,
+    fr: `Continuer`,
+    en: `Continue`,
   },
   MODAL_IGNORE: { fr: 'Poursuivre', en: 'Ignore' },
   MODAL_CORRECT: { fr: 'Corriger ma réponse', en: 'Correct' },
