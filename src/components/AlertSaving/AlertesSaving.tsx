fd498660d89262aa1fe7bc7b044e28ed231290d5
Renaud Genevois <renaud.genevois@insee.fr>
2023-03-29 17:30:54 +0200

    remove info saving

--- src/components/AlertSaving/AlertesSaving.tsx
+++ src/components/AlertSaving/AlertesSaving.tsx
@@ -5,21 +5,10 @@ export function AlertesSaving(props: OrchestratedElement) {
 	const { savingFailure } = props;
 	if (savingFailure) {
 		const { status } = savingFailure;
-		if (status === 200) {
-			return (
-				<Alert
-					className='fr-mb-3w'
-					closable
-					description="Vos données sont sauvegardées."
-					severity="info"
-					small
-				/>
-			);
-		}
 		if (status === 500) {
 			return (
 				<Alert
-					className='fr-mb-3w'
+					className="fr-mb-3w"
 					closable
 					description="Vos données n'ont pu être sauvegardées, veuillez réessayer ou contacter l'assistance."
 					severity="error"
