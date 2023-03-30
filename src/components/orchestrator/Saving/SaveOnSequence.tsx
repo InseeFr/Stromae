724d1c4b742caafcb7815153b75de752e6da961d
Renaud Genevois <renaud.genevois@insee.fr>
2023-03-30 09:04:03 +0200

    rename var conf for react app
    Signed-off-by: Renaud Genevois <renaud.genevois@insee.fr>

--- src/components/orchestrator/Saving/SaveOnSequence.tsx
+++ src/components/orchestrator/Saving/SaveOnSequence.tsx
@@ -4,13 +4,11 @@ import { OrchestratedElement } from '../Orchestrator';
 import { useSaving } from './useSaving';
 import { isComponentsContainSequence } from '../../../lib/commons/isComponentscontainSequence';
 
-export const SAVING_STRATEGY = process.env.SAVING_STRATEGY || 'partial'; // or complete
-
 export function SaveOnSequence(props: PropsWithChildren<OrchestratedElement>) {
 	const { children, ...rest } = props;
 	const { currentChange, getData, getComponents } = rest;
 
-	const save = useSaving({ strategy: SAVING_STRATEGY, currentChange, getData });
+	const save = useSaving({ currentChange, getData });
 
 	useEffect(
 		function () {
