90ef66a48c36ad3b95107d88e14923105f00cfa1
Renaud Genevois <renaud.genevois@insee.fr>
2023-04-13 15:33:40 +0200

    tabulate

--- src/components/ContinueOrRestart/ModalContinueOrRestart.tsx
+++ src/components/ContinueOrRestart/ModalContinueOrRestart.tsx
@@ -1,6 +1,6 @@
-import { Button } from '@codegouvfr/react-dsfr/Button';
-import { ButtonsGroup } from '@codegouvfr/react-dsfr/ButtonsGroup';
+import { useRef } from 'react';
 import { fr } from '@codegouvfr/react-dsfr';
+import { ModalDsfr } from './ModalDsfr';
 
 export function ModalContinueOrRestart(props: {
 	display: boolean;
@@ -16,67 +16,44 @@ export function ModalContinueOrRestart(props: {
 		currentPage,
 		iteration,
 	} = props;
+
+	const last = useRef<HTMLButtonElement>(null);
+
+	function continu() {
+		goToPage({ page: currentPage, iteration });
+		close();
+	}
+
+	function restart() {
+		goToPage({ page: '1', iteration });
+		close();
+	}
+
 	if (display) {
 		return (
-			<dialog id="fr-modal-1" className={fr.cx('fr-modal', 'fr-modal--opened')}>
-				<div
-					className={fr.cx(
-						'fr-container',
-						'fr-container--fluid',
-						'fr-container-md'
-					)}
-				>
-					<div className={fr.cx('fr-grid-row', 'fr-grid-row--center')}>
-						<div className={fr.cx('fr-col-12', 'fr-col-md-8', 'fr-col-lg-6')}>
-							<div className={fr.cx('fr-modal__body')}>
-								<div className={fr.cx('fr-modal__header')}>
-									<Button
-										className={fr.cx('fr-link--close', 'fr-link')}
-										onClick={close}
-										priority="tertiary no outline"
-									>
-										Fermer
-									</Button>
-								</div>
-								<div className="fr-modal__content">
-									<h1 className={fr.cx('fr-modal__title')}>Bienvenue</h1>
-									<p>
-										Vous avez déjà commencé à renseigner le questionnaire.
-										Souhaitez-vous reprendre la vous en étiez ou revenir à la
-										première page ?
-									</p>
-									<ButtonsGroup
-										buttons={[
-											{
-												children: 'Revenir à la première page',
-												nativeButtonProps: {
-													onClick: () => {
-														goToPage({ page: '1', iteration });
-														close();
-													},
-												},
-												priority: 'secondary',
-											},
-											{
-												children: 'Reprendre là où j’en étais',
-												nativeButtonProps: {
-													onClick: () => {
-														goToPage({ page: currentPage, iteration });
-														close();
-													},
-												},
-												priority: 'primary',
-											},
-										]}
-										alignment="right"
-										inlineLayoutWhen="md and up"
-									/>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</dialog>
+			<ModalDsfr
+				id="start-or-continue"
+				close={close}
+				last={last.current as HTMLElement}
+			>
+				<h1 className={fr.cx('fr-modal__title')}>Bienvenue</h1>
+				<p>
+					Vous avez déjà commencé à renseigner le questionnaire. Souhaitez-vous
+					reprendre la vous en étiez ou revenir à la première page ?
+				</p>
+				<ul className="fr-btns-group fr-btns-group--inline-md">
+					<li>
+						<button className="fr-btn fr-btn--secondary" onClick={restart}>
+							Revenir à la première page
+						</button>
+					</li>
+					<li>
+						<button className="fr-btn" onClick={continu} ref={last}>
+							Reprendre la où j'en était
+						</button>
+					</li>
+				</ul>
+			</ModalDsfr>
 		);
 	}
 	return null;
