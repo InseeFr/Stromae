bf3c0f5039364a55e818a361eae7ddb845aae01e
Julia Dirand <julia.dirand@insee.fr>
2023-04-17 16:03:54 +0200

    replace header and add display

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,9 +1,9 @@
 import {
-	useEffect,
-	useState,
-	useRef,
-	useContext,
-	PropsWithChildren,
+  useEffect,
+  useState,
+  useRef,
+  useContext,
+  PropsWithChildren,
 } from 'react';
 import { FooterType } from '../footer/FooterType';
 import { HeaderType } from '../header/HeaderType';
@@ -15,51 +15,53 @@ import SkipLinks from '@codegouvfr/react-dsfr/SkipLinks';
 import { Layout as LayoutSkeleton } from '../skeleton/Layout';
 import { Main } from './Main';
 import { loadSourceDataContext } from '../loadSourceData/LoadSourceDataContext';
+import { Display } from '@codegouvfr/react-dsfr/Display'
 
 type LayoutProps = {};
 
 const defaultLinks = [
-	{
-		anchor: '#contenu',
-		label: 'Contenu',
-	},
+  {
+    anchor: '#contenu',
+    label: 'Contenu',
+  },
 ];
 
 export function Layout({ children }: PropsWithChildren<LayoutProps>) {
-	const alreadyLoad = useRef(false);
-	const [header, setHeader] = useState<HeaderType | undefined>(undefined);
-	const [footer, setFooter] = useState<FooterType | undefined>(undefined);
-	const { getMetadata } = useContext(loadSourceDataContext);
+  const alreadyLoad = useRef(false);
+  const [header, setHeader] = useState<HeaderType | undefined>(undefined);
+  const [footer, setFooter] = useState<FooterType | undefined>(undefined);
+  const { getMetadata } = useContext(loadSourceDataContext);
 
-	useEffect(
-		function () {
-			if (getMetadata && !alreadyLoad.current) {
-				alreadyLoad.current = true;
-				(async function () {
-					const data = await getMetadata();
-					if (data) {
-						const { Header, Footer } = data;
-						setHeader(Header);
-						setFooter(Footer);
-					} else throw new Error('metadata missing.');
-				})();
-			}
-		},
-		[getMetadata, alreadyLoad]
-	);
+  useEffect(
+    function () {
+      if (getMetadata && !alreadyLoad.current) {
+        alreadyLoad.current = true;
+        (async function () {
+          const data = await getMetadata();
+          if (data) {
+            const { Header, Footer } = data;
+            setHeader(Header);
+            setFooter(Footer);
+          } else throw new Error('metadata missing.');
+        })();
+      }
+    },
+    [getMetadata, alreadyLoad]
+  );
 
-	if (header && footer) {
-		return (
-			<>
-				<SkipLinks links={defaultLinks} />
-				<HeaderAuth>
-					<Header header={header} />
-				</HeaderAuth>
-				<Banner />
-				<Main id="contenu">{children}</Main>
-				<Footer footer={footer} />
-			</>
-		);
-	}
-	return <LayoutSkeleton />;
+  if (header && footer) {
+    return (
+      <>
+        <SkipLinks links={defaultLinks} />
+        <HeaderAuth>
+          <Header header={header} />
+        </HeaderAuth>
+        <Banner />
+        <Main id="contenu">{children}</Main>
+        <Footer footer={footer} />
+        <Display />
+      </>
+    );
+  }
+  return <LayoutSkeleton />;
 }
