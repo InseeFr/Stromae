bb3571a951fc47dfad5026301abff931d8f9f6f6
Matt Clark <clarkm02@gmail.com>
2023-04-17 18:56:51 +0200

    Refactor and maintain separation of responsabilities

--- src/components/postSubmit/PostSubmitSurvey.tsx
+++ src/components/postSubmit/PostSubmitSurvey.tsx
@@ -5,7 +5,7 @@ import { useContext } from 'react';
 import { loadSourceDataContext } from '../loadSourceData/LoadSourceDataContext';
 import { Skeleton } from '@mui/material';
 import { ReactComponent as Confirmation } from '../../assets/confirmation.svg';
-
+import AdditionalInformation from './AdditionalInformation';
 
 export function PostSubmitSurvey() {
   const navigate = useNavigate();
@@ -14,6 +14,7 @@ export function PostSubmitSurvey() {
   const { getMetadata } = useContext(loadSourceDataContext);
   const metadata = useRemote<any>(getMetadata, navigateError);
   const submit = metadata?.Submit || false;
+  const { DescriptionAdditional } = submit;
 
   function navigateError() {
     navigate('/');
@@ -23,8 +24,6 @@ export function PostSubmitSurvey() {
   // to do: add dynamic submission date and time
   const submissionDate = "16/01/2022 à 10h30"
 
-  const hasAdditionalInformation = submit && submit.TitleAdditionalInformation && (submit.Feedback || submit.Results)
-
   if (!metadata) {
     return <Skeleton />;
   }
@@ -33,8 +32,7 @@ export function PostSubmitSurvey() {
     <div className="fr-grid-row fr-my-6w fr-my-md-12w">
       <div className="fr-col-12 fr-col-lg-6 fr-col-offset-lg-1">
         <h1>L'Insee vous remercie pour votre collaboration à cette enquête.</h1>
-        <p className="fr-text--lead">Vos réponses ont été envoyées le {submissionDate}. {submit.DescriptionAdditional} </p>
-
+        <p className="fr-text--lead">Vos réponses ont été envoyées le {submissionDate}. {DescriptionAdditional} </p>
         <Button>
           Télécharger l'accusé de réception
         </Button>
@@ -42,27 +40,7 @@ export function PostSubmitSurvey() {
       <div className="fr-col-lg-3 fr-col-offset-lg-1 fr-col-12 fr-col--middle fr-btns-group--center">
         <Confirmation />
       </div>
-      {hasAdditionalInformation &&
-        <><div className="fr-col-12 fr-col-lg-6 fr-mt-9w fr-col-offset-lg-1">
-          <h2>{submit.TitleAdditionalInformation}</h2>
-        </div>
-          {submit && submit.Feedback &&
-            <div className="fr-p-md-3w fr-col-12 fr-col-lg-5 fr-col-offset-lg-1">
-              <h6>{submit.Feedback.title}</h6>
-              {submit.Feedback.link.href &&
-                <a title={`${submit.Feedback.link.label} - ouvre une nouvelle fenêtre`} href={submit.Feedback.link.href}>
-                  {submit.Feedback.link.imageSrc ? <img src={submit.Feedback.link.imageSrc} alt="" /> : submit.Feedback.link.label}
-                </a>}
-            </div>
-          }
-          {submit && submit.Results &&
-            <div className="fr-p-md-3w fr-col-12 fr-col-lg-5 fr-col-offset-lg-1 fr-mt-5w fr-mt-md-0 fr-p-lg-3w">
-              <h6>{submit.Results.title}</h6>
-              <a title={`${submit.Results.link.label} - ouvre une nouvelle fenêtre`} href={submit.Results.link.href} target="_blank" rel="noopener">{submit.Results.link.label}</a>
-            </div>
-          }
-        </>
-      }
+      <AdditionalInformation submit={submit}/>
     </div>
   );
 }
