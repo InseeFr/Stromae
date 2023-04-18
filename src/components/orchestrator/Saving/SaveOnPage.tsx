1d1ae1e6e7a38518288baf21aa82e41ab32b5e05
Renaud Genevois <renaud.genevois@insee.fr>
2023-04-18 16:29:04 +0200

    sav on last submit

--- src/components/orchestrator/Saving/SaveOnPage.tsx
+++ src/components/orchestrator/Saving/SaveOnPage.tsx
@@ -1,37 +1,38 @@
-import { useCallback, useState , PropsWithChildren } from 'react';
-
+import { useCallback, useState, PropsWithChildren } from 'react';
 import { OrchestratedElement, SavingFailure } from '../../../typeStromae/type';
 import { CloneElements } from '../CloneElements';
-
 import { useSaving } from './useSaving';
 
 export function SaveOnPage(props: PropsWithChildren<OrchestratedElement>) {
 	const { children, ...rest } = props;
-	const { goNextPage = () => null, currentChange, getData, pageTag } = rest;
+	const {
+		goNextPage = () => null,
+		currentChange,
+		getData,
+		pageTag,
+		isLastPage,
+	} = rest;
 
 	const [savingFailure, setSavingFailure] = useState<SavingFailure>();
-	const save = useSaving({ currentChange, getData, pageTag });
+	const save = useSaving({ currentChange, getData, pageTag, isLastPage });
 
-	const handleNextPage = useCallback(
-		() => {
-			(async function () {
-				try {
-					setSavingFailure(undefined);
-					const somethingToSave = await save();
+	const handleNextPage = useCallback(() => {
+		(async function () {
+			try {
+				setSavingFailure(undefined);
+				const somethingToSave = await save();
 
-					if (somethingToSave) {
-						setSavingFailure({ status: 200 });
-					}
-					goNextPage();
-				} catch (e) {
-					// eslint-disable-next-line no-console
-					console.error(e);
-					setSavingFailure({ status: 500 });
+				if (somethingToSave) {
+					setSavingFailure({ status: 200 });
 				}
-			})();
-		},
-		[goNextPage, save]
-	);
+				goNextPage();
+			} catch (e) {
+				// eslint-disable-next-line no-console
+				console.error(e);
+				setSavingFailure({ status: 500 });
+			}
+		})();
+	}, [goNextPage, save]);
 
 	return (
 		<CloneElements<OrchestratedElement>
