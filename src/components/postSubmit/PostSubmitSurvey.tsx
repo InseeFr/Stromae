b305ad21c131c5ed6e927a7938d8b18560b2f22d
Renaud Genevois <renaud.genevois@insee.fr>
2023-04-19 14:22:44 +0200

    date and type

--- src/components/postSubmit/PostSubmitSurvey.tsx
+++ src/components/postSubmit/PostSubmitSurvey.tsx
@@ -1,27 +1,44 @@
+import { PropsWithChildren, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
+import moment from 'moment';
+import 'moment/locale/fr';
 import { Button } from '@codegouvfr/react-dsfr/Button';
 import { useRemote } from '../orchestrator/useRemote';
-import { useContext } from 'react';
 import { loadSourceDataContext } from '../loadSourceData/LoadSourceDataContext';
 import { Skeleton } from '@mui/material';
 import { ReactComponent as Confirmation } from '../../assets/confirmation.svg';
 import AdditionalInformation from './AdditionalInformation';
+import {
+	MetadataSurvey,
+	OrchestratedElement,
+	SurveyUnitData,
+} from '../../typeStromae/type';
 
-export function PostSubmitSurvey() {
+function parseDate(date?: number) {
+	if (date !== undefined) {
+		return moment(date).locale('fr').format('Do MMMM YYYY [à] h:mm');
+	}
+	return '';
+}
+
+export function PostSubmitSurvey(
+	props: PropsWithChildren<OrchestratedElement>
+) {
 	const navigate = useNavigate();
 
-	const { getMetadata } = useContext(loadSourceDataContext);
-	const metadata = useRemote<any>(getMetadata, navigateError);
-	const submit = metadata?.Submit || false;
-	const { DescriptionAdditional } = submit;
+	const { getMetadata, getSurveyUnitData } = useContext(loadSourceDataContext);
+	const metadata = useRemote<MetadataSurvey>(getMetadata, navigateError);
+	const surveyUnitData = useRemote<SurveyUnitData>(
+		getSurveyUnitData,
+		navigateError
+	);
+	const submit = metadata?.Submit;
+	const submissionDate = parseDate(surveyUnitData?.stateData?.date);
+	const DescriptionAdditional = submit?.DescriptionAdditional ?? null;
 
 	function navigateError() {
 		navigate('/');
 	}
-
-	// TODO: onClick : dowload confirmation receipt
-	// TODO: add dynamic submission date and time
-	const submissionDate = '16/01/2022 à 10h30';
 
 	if (!metadata) {
 		return <Skeleton />;
@@ -32,8 +49,8 @@ export function PostSubmitSurvey() {
 			<div className="fr-col-12 fr-col-lg-6 fr-col-offset-lg-1">
 				<h1>L'Insee vous remercie pour votre collaboration à cette enquête.</h1>
 				<p className="fr-text--lead">
-					Vos réponses ont été envoyées le {submissionDate}.{' '}
-					{DescriptionAdditional}{' '}
+					Vos réponses ont été envoyées le {submissionDate}.
+					{DescriptionAdditional}
 				</p>
 				<Button>Télécharger l'accusé de réception</Button>
 			</div>
