0b08bd4490c82e0ba2919f67595da7cd1acc11a9
Matt Clark <clarkm02@gmail.com>
2023-07-17 13:32:50 +0200

    fixing spacing for complementary components

--- src/pages/questionnaire/Questionnaire.tsx
+++ src/pages/questionnaire/Questionnaire.tsx
@@ -40,7 +40,7 @@ export function Questionnaire(props: QuestionnaireProps) {
 							<Formulaire />
 							<Continuer />
 						</Grid>
-							<ComplementaryComponents />
+						<ComplementaryComponents />
 					</Orchestrator>
 				</Layout>
 			</LoadFromApi>
