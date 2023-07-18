b7088dd0d86cc45ebcac3c6b55bb30c03c1cbc0e
Matt Clark <clarkm02@gmail.com>
2023-07-18 11:45:05 +0200

    remove useState and useEffect

--- src/components/ComponentsRenderer/ComponentsRenderer.tsx
+++ src/components/ComponentsRenderer/ComponentsRenderer.tsx
@@ -1,30 +1,20 @@
 import * as lunatic from '@inseefr/lunatic';
-import { useEffect, useState } from 'react';
 import { ComponentType } from '../../typeLunatic/type-source';
 import { OrchestratedElement } from '../../typeStromae/type';
 import { LunaticComponentContainer } from '../formulaire/LunaticComponentContainer';
 
 export function ComponentsRenderer(props: OrchestratedElement) {
 	const { getComponents, currentErrors, disabled = false, only, except } = props;
-	const [components, setComponents] = useState<Array<ComponentType>>([]);
+	const components = getComponents?.().filter((component) => {
+		const { componentType } = component;
 
-	useEffect(() => {
-		if (typeof getComponents !== 'function') {
-			return;
-		}
-		const receivedComponents = getComponents();
-
-		setComponents(receivedComponents.filter((component) => {
-			const { componentType } = component;
-
-			return (!only || (only && only.includes(componentType))) &&
-			(!except || (except && !(except.includes(componentType))))
-		}));
-	}, [getComponents, except, only]);
+		return (!only || (only && only.includes(componentType))) &&
+		(!except || (except && !(except.includes(componentType))))
+	})
 
 	return (
 		<>
-			{components.map((component: ComponentType) => {
+			{components?.map((component: ComponentType) => {
 				const { componentType, id } = component;
 
 				if (componentType in lunatic) {
