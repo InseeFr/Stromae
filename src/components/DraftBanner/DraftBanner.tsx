41b76d7307642fe810ae78d3dd5bb5e87dbbdd9f
Debbabi-Ext Aymen <WDMJ3W@ad.insee.intra>
2023-08-17 09:36:03 +0200

    fix dsfr classes

--- src/components/DraftBanner/DraftBanner.tsx
+++ src/components/DraftBanner/DraftBanner.tsx
@@ -2,6 +2,7 @@ import { PropsWithChildren, useState, useEffect } from 'react';
 import { makeStyles } from "@codegouvfr/react-dsfr/tss";
 import { Badge } from '@codegouvfr/react-dsfr/Badge';
 import { OrchestratedElement } from '../../typeStromae/type';
+import { fr } from '@codegouvfr/react-dsfr';
 
 const useStyles = makeStyles()({
     container: {
@@ -31,17 +32,23 @@ export function DraftBanner(props: PropsWithChildren<OrchestratedElement>) {
         address = `Recensement du 80, rue des morillons, 75014 Paris `;
     }
     return (
-        <div className={cx(classes.container, "fr-col-12", "fr-p-2w", "fr-mb-1w")}>
+        <div className={cx(classes.container, "fr-col-12", "fr-py-2w", "fr-mb-1w")}>
             <div className="fr-container">
-                {address && <div className="fr-p-1v">
-                    <span className="fr-text--bold">{address}</span>
-                    <Badge className={"fr-mx-2w"}>BROUILLON</Badge>
+            <div className="fr-grid-column">
+                {address && <div className={fr.cx("fr-grid-row--no-gutters","fr-grid-row")}>
+                    <div className=''>
+                    <span className="fr-text--bold fr-mr-2w">{address}</span>
+                    </div>
+                    <div className=''>
+                    <Badge>BROUILLON</Badge>
+                    </div>
                 </div>}
-                <div className={cx("fr-p-1v")}>
+                <div className={"fr-p-1v"}>
                     {!address && <Badge className="fr-mx-2w">BROUILLON</Badge>}
-                    {saved ? <span><i className={cx("fr-icon-checkbox-circle-fill", "fr-label--success", "fr-mr-1v")} />Brouillon enregistré.</span> :
+                    {saved ? <span><i className="fr-icon-checkbox-circle-fill fr-label--success fr-mr-1v" />Brouillon enregistré.</span> :
                         <span>Vos réponses sont enregistrées automatiquement à chaque chargement de page.</span>}
                 </div>
+            </div>
             </div>
         </div>
     );
