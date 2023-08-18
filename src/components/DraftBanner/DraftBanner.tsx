0accdf78140b5f2d6860b554eefd3dc8c9a547c1
Matthew Clark <matthew.clark@insee.fr>
2023-08-18 12:00:25 +0200

    add savingFailure to useEffect

--- src/components/DraftBanner/DraftBanner.tsx
+++ src/components/DraftBanner/DraftBanner.tsx
@@ -1,55 +1,65 @@
 import { PropsWithChildren, useState, useEffect } from 'react';
-import { makeStyles } from "@codegouvfr/react-dsfr/tss";
+import { makeStyles } from '@codegouvfr/react-dsfr/tss';
 import { Badge } from '@codegouvfr/react-dsfr/Badge';
 import { OrchestratedElement } from '../../typeStromae/type';
 import { fr } from '@codegouvfr/react-dsfr';
 
 const useStyles = makeStyles()({
-    container: {
-        borderBottom: "1px solid var(--border-default-grey)",
-    },
+	container: {
+		borderBottom: '1px solid var(--border-default-grey)',
+	},
 });
 
-
 export function DraftBanner(props: PropsWithChildren<OrchestratedElement>) {
-    const { getData, waiting, savingFailure } = props;
-    const { classes, cx } = useStyles();
-    const [saved, setSaved] = useState(false);
-    const timer = 2000;
-    let address = '';
+	const { getData, waiting, savingFailure } = props;
+	const { classes, cx } = useStyles();
+	const [saved, setSaved] = useState(false);
+	const timer = 2000;
+	let address = '';
 
-    useEffect(() => {
-        if (waiting && Boolean(!savingFailure)) {
-            setSaved(true);
-            setTimeout(() => {
-                setSaved(false);
-            }, timer);
-        }
-    }, [waiting]);
-    // TO DO: get data from personalization annd remove the getData method props
-    // TO DO: mobile screen responsive
-    if (getData) {
-        address = `Recensement du 80, rue des morillons, 75014 Paris `;
-    }
-    return (
-        <div className={cx(classes.container, "fr-col-12", "fr-py-2w", "fr-mb-1w")}>
-            <div className="fr-container">
-            <div className="fr-grid-column">
-                {address && <div className={fr.cx("fr-grid-row--no-gutters","fr-grid-row")}>
-                    <div className=''>
-                    <span className="fr-text--bold fr-mr-2w">{address}</span>
-                    </div>
-                    <div className=''>
-                    <Badge>BROUILLON</Badge>
-                    </div>
-                </div>}
-                <div className={"fr-p-1v"}>
-                    {!address && <Badge className="fr-mx-2w">BROUILLON</Badge>}
-                    {saved ? <span><i className="fr-icon-checkbox-circle-fill fr-label--success fr-mr-1v" />Brouillon enregistré.</span> :
-                        <span>Vos réponses sont enregistrées automatiquement à chaque chargement de page.</span>}
-                </div>
-            </div>
-            </div>
-        </div>
-    );
+	useEffect(() => {
+		if (waiting && Boolean(!savingFailure)) {
+			setSaved(true);
+			setTimeout(() => {
+				setSaved(false);
+			}, timer);
+		}
+	}, [waiting, savingFailure]);
+	// TO DO: get data from personalization annd remove the getData method props
+	// TO DO: mobile screen responsive
+	if (getData) {
+		address = `Recensement du 80, rue des morillons, 75014 Paris `;
+	}
+	return (
+		<div className={cx(classes.container, 'fr-col-12', 'fr-py-2w', 'fr-mb-1w')}>
+			<div className="fr-container">
+				<div className="fr-grid-column">
+					{address && (
+						<div className={fr.cx('fr-grid-row--no-gutters', 'fr-grid-row')}>
+							<div className="">
+								<span className="fr-text--bold fr-mr-2w">{address}</span>
+							</div>
+							<div className="">
+								<Badge>BROUILLON</Badge>
+							</div>
+						</div>
+					)}
+					<div className={'fr-p-1v'}>
+						{!address && <Badge className="fr-mx-2w">BROUILLON</Badge>}
+						{saved ? (
+							<span>
+								<i className="fr-icon-checkbox-circle-fill fr-label--success fr-mr-1v" />
+								Brouillon enregistré.
+							</span>
+						) : (
+							<span>
+								Vos réponses sont enregistrées automatiquement à chaque
+								chargement de page.
+							</span>
+						)}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
