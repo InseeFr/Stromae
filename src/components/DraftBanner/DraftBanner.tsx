341c16933fd490614abd57d360f8b070f69f7417
Julia Dirand <julia.dirand@insee.fr>
2023-08-21 10:17:45 +0200

    fix-alignement

--- src/components/DraftBanner/DraftBanner.tsx
+++ src/components/DraftBanner/DraftBanner.tsx
@@ -11,20 +11,20 @@ const useStyles = makeStyles()({
 		borderBottom: '1px solid var(--border-default-grey)',
 	},
 	badgeContainer: {
-		minWidth: 'fit-content'
+		minWidth: 'fit-content',
 	},
 	addressRow: {
-		"@media (min-width: 48em)": {
-			flexDirection: "row-reverse"
-		}
-	}
+		'@media (min-width: 48em)': {
+			flexDirection: 'row-reverse',
+		},
+	},
 });
 
 export function DraftBanner(props: PropsWithChildren<OrchestratedElement>) {
 	const { waiting, savingFailure, personalization } = props;
 	const { classes, cx } = useStyles();
 	const [saved, setSaved] = useState(false);
-	const timer = 5000;
+	const timer = 2000;
 	let address = '';
 
 	if (personalization?.bannerAddress) {
@@ -45,9 +45,30 @@ export function DraftBanner(props: PropsWithChildren<OrchestratedElement>) {
 		<div className={cx(classes.container, 'fr-col-12', 'fr-py-2w', 'fr-mb-1w')}>
 			<div className="fr-container">
 				<div className="fr-grid-column">
-					<div className={fr.cx('fr-grid-row--no-gutters', 'fr-grid-row')}>
-						<div className={cx(classes.addressRow, 'fr-grid-row--no-gutters', 'fr-grid-row')}>
-							<div className={cx(classes.badgeContainer,'fr-col-12', 'fr-col-md-1', 'fr-mb-1w', 'fr-mr-1w' )}>
+					<div
+						className={fr.cx(
+							'fr-grid-row--no-gutters',
+							'fr-grid-row',
+							'fr-grid-row--middle'
+						)}
+					>
+						<div
+							className={cx(
+								classes.addressRow,
+								'fr-grid-row--no-gutters',
+								'fr-grid-row'
+							)}
+						>
+							<div
+								className={cx(
+									classes.badgeContainer,
+									'fr-col-12',
+									'fr-col-md-1',
+									'fr-mr-1w',
+									'fr-mb-1w',
+									'fr-mb-md-0'
+								)}
+							>
 								<Badge>BROUILLON</Badge>
 							</div>
 							<BannerAddress address={address} />
