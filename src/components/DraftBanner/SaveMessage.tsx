461510df0d411ee1e17e220e41f71632dce05562
Julia Dirand <julia.dirand@insee.fr>
2023-08-21 11:21:33 +0200

    improve-animation

--- src/components/DraftBanner/SaveMessage.tsx
+++ src/components/DraftBanner/SaveMessage.tsx
@@ -1,28 +1,33 @@
-import { fr } from "@codegouvfr/react-dsfr";
+import { fr } from '@codegouvfr/react-dsfr';
 import { makeStyles } from '@codegouvfr/react-dsfr/tss';
 
 type Props = {
 	saved: boolean;
-}
+};
 
 const useStyles = makeStyles()({
 	root: {
 		opacity: 0,
-		transition: 'opacity 2s'
-	}
+		transition: 'opacity ease-in-out 2s',
+	},
 });
 
-
 export function SaveMessage(props: Props) {
-	const {saved} = props;
+	const { saved } = props;
 	const { classes, cx } = useStyles();
 
 	if (saved) {
 		return (
-			<span className={cx(classes.root)}><i className="fr-icon-checkbox-circle-fill fr-label--success fr-mr-1v" />Brouillon enregistré.</span>
-		)
+			<span className={cx(classes.root)}>
+				<i className="fr-icon-checkbox-circle-fill fr-label--success fr-mr-1v" />
+				Brouillon enregistré.
+			</span>
+		);
 	}
 	return (
-		<span className={fr.cx('fr-col-12', 'fr-col-md-10')}>Vos réponses sont enregistrées automatiquement à chaque chargement de page.</span>
-	)
+		<span className={fr.cx('fr-col-12', 'fr-col-md-10')}>
+			Vos réponses sont enregistrées automatiquement à chaque chargement de
+			page.
+		</span>
+	);
 }
