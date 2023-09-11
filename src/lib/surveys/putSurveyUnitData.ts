8654941da4dc426939bbb41764f8cfabc087defd
Renaud Genevois <renaud.genevois@insee.fr>
2023-09-11 10:49:56 +0200

    remove token and let axa do the job

--- src/lib/surveys/putSurveyUnitData.ts
+++ src/lib/surveys/putSurveyUnitData.ts
@@ -1,13 +1,9 @@
 import { DataVariables } from '../../typeStromae/type';
-import { authenticatedPutRequest } from '../commons/axios-utils';
+import { putRequest } from '../commons/axios-utils';
 
 import { surveyUnitDataUrl } from './api';
 
 export const putSurveyUnitData =
-	(domain: string) => (data: DataVariables, unit: string, token: string) => {
-		return authenticatedPutRequest<DataVariables>(
-			surveyUnitDataUrl(domain, unit),
-			data,
-			token
-		);
+	(domain: string) => (data: DataVariables, unit: string) => {
+		return putRequest<DataVariables>(surveyUnitDataUrl(domain, unit), data);
 	};
