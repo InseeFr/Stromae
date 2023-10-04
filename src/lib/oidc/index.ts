af4cea5d0b580aa2239c07e8ab770265a5d8c868
Laurent Caouissin <caouissin.laurent@gmail.com>
2023-10-04 17:25:51 +0200

    chore: rename file

--- src/lib/oidc/index.ts
+++ src/lib/oidc/index.ts
@@ -1,4 +1,4 @@
 export { AuthSecure } from './AuthSecure';
-export { useAccessToken } from './useAccessToken ';
+export { useAccessToken } from './useAccessToken';
 export { useAuth } from './useAuth';
 export { useAuthUser } from './useAuthUser';
