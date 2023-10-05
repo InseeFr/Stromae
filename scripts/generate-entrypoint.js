81005802ce828bd9ea101c39af8fcc573decb1a3
Laurent Caouissin <caouissin.laurent@gmail.com>
2023-10-05 10:42:54 +0200

    refactor: reading env variable

---
+++ scripts/generate-entrypoint.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+fs.readFile('.env', 'utf8', function (_, contents) {
+  const content = contents
+    .split('\n')
+    .filter((line) => !line.startsWith('#'))
+    .map((line) => line.split('='))
+    .filter((data) => data.length === 2)
+    .map(
+      ([key]) =>
+        `echo "self._env_['${key}'] = '\$${key}';" >> /usr/share/nginx/html/env-config.js`
+    );
+
+  const fullFile = ['#!/bin/sh', ...content, 'exec "$@"'].join('\n');
+  fs.writeFileSync('entrypoint.sh', fullFile, 'utf8');
+});
