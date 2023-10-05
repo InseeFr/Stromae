2c10e2adcb3b0ec6ad8feb3a71a104c9eb8f443c
Laurent Caouissin <caouissin.laurent@gmail.com>
2023-10-05 09:37:38 +0200

    feat: routing to visu in noAuth mode

--- src/pages/portail/RoutingPortail.tsx
+++ src/pages/portail/RoutingPortail.tsx
@@ -1,14 +1,16 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { environment } from '../../utils/read-env-vars';
+import { AuthTypeEnum, environment } from '../../utils/read-env-vars';
 
-const { DEFAULT_SURVEY } = environment;
+const { DEFAULT_SURVEY, AUTH_TYPE } = environment;
 
 export function RoutingPortail() {
 	const navigate = useNavigate();
 
 	useEffect(() => {
-		if (DEFAULT_SURVEY) {
+		if (AUTH_TYPE === AuthTypeEnum.None) {
+			navigate(`/visualize`);
+		} else if (DEFAULT_SURVEY) {
 			navigate(`/questionnaire/${DEFAULT_SURVEY}`);
 		} else {
 			navigate(`/404`);
