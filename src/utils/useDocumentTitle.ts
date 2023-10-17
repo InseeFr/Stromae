0257178740935dc21c6b5e133b1886915b641cb8
Renaud Genevois <renaud.genevois@insee.fr>
2023-10-17 13:45:55 +0200

    title from metadata

--- src/utils/useDocumentTitle.ts
+++ src/utils/useDocumentTitle.ts
@@ -1,9 +1,17 @@
 import { useEffect } from 'react';
+import { useMetadata } from '../hooks/useMetadata';
+
+function concat({ title, header }: { title?: string; header?: string }) {
+	return `${title ?? ''}${title && header ? '|' : ''}${header ?? ''}`;
+}
 
 export function useDocumentTitle(title?: string | null) {
+	const metadata = useMetadata();
+	const header = metadata?.Header?.serviceTitle as string;
+
 	useEffect(() => {
 		if (title) {
-			document.title = title;
+			document.title = concat({ title, header });
 		}
-	}, [title]);
+	}, [title, metadata, header]);
 }
