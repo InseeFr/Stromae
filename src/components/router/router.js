45ac4c3035a6c4f0002c3dd0c23e7323a1232537
Laurent Caouissin <caouissin.laurent@gmail.com>
2023-11-09 16:21:06 +0100

    feat: disabled vizu route according to env variable

--- src/components/router/router.js
+++ src/components/router/router.js
@@ -1,9 +1,12 @@
 import { Redirect, Route, Switch } from 'react-router-dom';
+import { environment } from 'utils/read-env-vars';
 import { READ_ONLY } from '../../utils/constants';
 import secure from '../auth/hoc/hoc';
 import { OrchestratorManager } from '../orchestrator/manager';
 import { Visualizer } from '../orchestrator/visualizer';
 import { NotFound } from '../pages';
+
+const { VIZUALIZE_ENABLED } = environment;
 
 const Router = () => {
   return (
@@ -12,7 +15,7 @@ const Router = () => {
         path={`/:${READ_ONLY}?/questionnaire/:idQ/unite-enquetee/:idSU`}
         component={secure(OrchestratorManager)}
       />
-      <Route path='/visualize' component={Visualizer} />
+      {VIZUALIZE_ENABLED && <Route path='/visualize' component={Visualizer} />}
       <Route path='/404' component={NotFound} />
       <Redirect to='/404' />
     </Switch>
