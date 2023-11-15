c68e8e3ce59ea479c9062ac492aa6cf5281b3afa
Debbabi-Ext Aymen <WDMJ3W@ad.insee.intra>
2023-11-15 15:41:56 +0100

    read data from metadata.json

--- src/paradata/useParadata.ts
+++ src/paradata/useParadata.ts
@@ -1,49 +1,69 @@
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import { useMetadata } from '../hooks/useMetadata';
+
+
+export type Component = {
+	id: string;
+	events: string[];
+}
 
 async function mockApi(events: Array<unknown>) {
 	window.setTimeout(() => {
-		console.log('clean!', events);
+		console.log('clean : ', events);
 	}, 50);
 }
 
 export function useParadata() {
 	const map = useRef(new Map<string, null>());
-	const tampon = useRef<Array<{ type: string; timestamp: number }>>([]);
+	const tampon = useRef<Array<{ type: string; element: string; timestamp: number }>>([]);
+	const [components, setComponents] = useState<[Component] | []>([]);
 	const metadata = useMetadata();
 
-	const cally = async () => {
-		// console.log('Array.isArray(tampon.current), tampon.current');
-		tampon.current.push({ type: 'blur', timestamp: new Date().getTime() });
-
+	const cally = async (e: Event) => {
+		tampon.current.push({ type: e.type, element: (e.target as HTMLInputElement).id, timestamp: new Date().getTime() });
 		await persist();
 	};
 
 	useEffect(() => {
 		if (metadata?.paradata) {
-			// TODO
+			setComponents(metadata?.paradata?.components);
 		}
 	}, [metadata]);
 
 	async function persist() {
-		if (tampon.current.length > 3) {
+		 if (tampon.current.length > 3) {
 			const temp = tampon.current;
 			tampon.current = [];
 			await mockApi(temp);
 
 			return true;
-		}
-		return false;
+		 }
+		 return false;
 	}
 
-	const elmt = document.getElementById('kkxuu8ov-nom');
-	if (elmt && !map.current.has('kkxuu8ov-nom')) {
-		map.current.set('kkxuu8ov-nom', null);
-		elmt.addEventListener('blur', cally);
+	const manageEventListner = (add: boolean) => {
+		if (components.length) {
+			components.forEach((component: Component) => {
+				const elmt = document.getElementById(component.id);
+				if (elmt && !map.current.has(component.id)) {
+					map.current.set(component.id, null);
+					component.events.forEach((ev: string) => {
+						if (add) {
+							elmt.addEventListener(ev, cally);
+						} else {
+							elmt.removeEventListener(ev, cally);
+						}
+					})
+				}
+			})
+		}
 	}
+
+
+	manageEventListner(true);
+
 
 	return () => {
-		document.getElementById('kkxuu8ov-nom')?.removeEventListener('blur', cally);
-		// vider un tampon
+		manageEventListner(false);
 	};
 }
