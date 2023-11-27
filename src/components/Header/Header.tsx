24264670e4c74298a949d1b1bead4426c4e2bcff
Julia Dirand <julia.dirand@insee.fr>
2023-11-27 11:32:11 +0100

    feat:upgrade lunatic

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -12,9 +12,6 @@ import type { ConvertContentType } from '../../utils/convertContent';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const iconContact = fr.cx('fr-icon-questionnaire-line');
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const iconContact = fr.cx('fr-icon-questionnaire-line');
-
 function getAuthLabel(isAuthenticated: boolean): string {
 	if (isAuthenticated) {
 		return 'Me déconnecter';
