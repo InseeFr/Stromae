210b2ae8ab1d9e47797dac8d0d4f46bec2844656
Renaud Genevois <renaud.genevois@insee.fr>
2023-11-28 12:16:59 +0100

    simplification & promise with moize

--- src/lib/surveys/getSurvey.ts
+++ src/lib/surveys/getSurvey.ts
@@ -1,16 +1,18 @@
 import { authenticatedGetRequest } from '../commons/axios-utils';
 import { LunaticSource } from '../../typeLunatic/type-source';
-
+import moize from 'moize';
 import { surveySource } from './api';
 
 /**
  * An endpoint to stromae-api for retrieving resource URLs.
  */
-export const getSurvey =
-	(BASE_URL: string) =>
-	(survey: string, token: string): Promise<LunaticSource> => {
-		return authenticatedGetRequest<LunaticSource>(
-			surveySource(BASE_URL, survey),
-			token
-		);
-	};
+export const getSurvey = (BASE_URL: string) =>
+	moize(
+		(survey: string, token: string): Promise<LunaticSource> => {
+			return authenticatedGetRequest<LunaticSource>(
+				surveySource(BASE_URL, survey),
+				token
+			);
+		},
+		{ isPromise: true }
+	);
--- src/lib/surveys/surveysApi.ts
+++ src/lib/surveys/surveysApi.ts
@@ -39,77 +39,13 @@ export interface SurveyApi {
 	getDepositiProof: (unit: string, token: string) => Promise<BlobPart>;
 }
 
-function transform(data: DataVariables) {
-	// il faudra peut etre tenir compte des autres propriétés (EDITED...)
-	return Object.entries(data).reduce((acc, [name, value]) => {
-		return {
-			...acc,
-			[name]: {
-				COLLECTED: value,
-				EDITED: null,
-				FORCED: null,
-				INPUTED: null,
-				PREVIOUS: null,
-			},
-		};
-	}, {});
-}
-
-function createManagedAPI(domain: string) {
-	let suData: SurveyUnitData;
-	const getSurveyUnitDataHTTP = getSurveyUnitData(domain);
-	const putSurveyUnitDataHTTP = putSurveyUnitData(domain);
-	const putSurveyUnitStateDataHTTP = putSurveyUnitStateData(domain);
-
-	return {
-		putSurveyUnitStateData: async (
-			state: StateData,
-			unit: string,
-			token: string
-		) => {
-			if (suData) {
-				suData.stateData = state;
-			}
-			await putSurveyUnitStateDataHTTP(state, unit, token);
-		},
-		putSurveyUnitData: async (
-			data: DataVariables,
-			unit: string,
-			token: string
-		) => {
-			if (suData) {
-				const variables = suData.data;
-				suData = {
-					...suData,
-					data: {
-						...variables,
-						COLLECTED: { ...variables.COLLECTED, ...transform(data) },
-					},
-				};
-			}
-			await putSurveyUnitDataHTTP(data, unit, token);
-		},
-		getSurveyUnitData: async (unit: string, token: string) => {
-			if (suData) {
-				return { ...suData };
-			}
-
-			// eslint-disable-next-line require-atomic-updates
-			suData = await getSurveyUnitDataHTTP(unit, token);
-			return suData;
-		},
-	};
-}
-
-const managed = createManagedAPI(DOMAIN);
-
 export const surveyApi: SurveyApi = {
 	getSurvey: getSurvey(DOMAIN),
 	getMetadataSurvey: getMetadataSurvey(DOMAIN),
-	getSurveyUnitData: managed.getSurveyUnitData,
+	getSurveyUnitData: getSurveyUnitData(DOMAIN),
 	getRequiredNomenclatures: getRequiredNomenclatures(DOMAIN),
 	getNomenclature: getNomenclature(DOMAIN),
-	putSurveyUnitData: managed.putSurveyUnitData,
-	putSurveyUnitStateData: managed.putSurveyUnitStateData,
+	putSurveyUnitData: putSurveyUnitData(DOMAIN),
+	putSurveyUnitStateData: putSurveyUnitStateData(DOMAIN),
 	getDepositiProof: getDepositProof(DOMAIN),
 };
