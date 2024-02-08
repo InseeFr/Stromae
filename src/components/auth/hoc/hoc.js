c04b68f8dbe4fb072ac8876167fbc4c1c5c68273
Laurent Caouissin <caouissin.laurent@gmail.com>
2024-02-08 11:53:16 +0100

    feat: avoid logout et decodeJwtToken when auth isn't enabled

--- src/components/auth/hoc/hoc.js
+++ src/components/auth/hoc/hoc.js
@@ -1,7 +1,11 @@
 import { decodeJwt } from 'oidc-spa';
+import { OIDC } from 'utils/constants';
 import { listenActivity } from 'utils/events';
 import { useAuth } from 'utils/hooks/useAuth';
+import { environment } from 'utils/read-env-vars';
 import { getLogoutUrl } from '../provider/component';
+
+const { AUTH_TYPE } = environment;
 
 const secure = (WrappedComponent) => {
   const Component = (props) => {
@@ -12,24 +16,27 @@ const secure = (WrappedComponent) => {
     const ReturnedComponent = <WrappedComponent {...otherProps} />;
 
     if (isUserLoggedIn) {
-      (async function callee() {
-        const { exp: expirationTime } = decodeJwt(oidc?.getTokens()?.idToken);
+      if (AUTH_TYPE === OIDC) {
+        (async function callee() {
+          const { exp: expirationTime } = decodeJwt(oidc?.getTokens()?.idToken);
 
-        const logoutTimeInMiliseconds = expirationTime * 1000 - Date.now();
+          const logoutTimeInMiliseconds = expirationTime * 1000 - Date.now();
 
-        const logoutTimeout = setTimeout(async () => {
-          console.log(
-            `No user activity detected during ${
-              logoutTimeInMiliseconds / 1000
-            } seconds. Logout user.`
-          );
-          await logout({ redirectTo: 'specific url', url: getLogoutUrl() });
-        }, logoutTimeInMiliseconds);
+          const logoutTimeout = setTimeout(async () => {
+            console.log(
+              `No user activity detected during ${
+                logoutTimeInMiliseconds / 1000
+              } seconds. Logout user.`
+            );
+            await logout({ redirectTo: 'specific url', url: getLogoutUrl() });
+          }, logoutTimeInMiliseconds);
 
-        await listenActivity();
-        clearTimeout(logoutTimeout);
-        callee();
-      })();
+          await listenActivity();
+          clearTimeout(logoutTimeout);
+          callee();
+        })();
+      }
+
       return ReturnedComponent;
     }
     login({
