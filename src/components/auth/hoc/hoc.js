62152da5b03765f47d5c9d84604d749da24442f7
Laurent Caouissin <caouissin.laurent@gmail.com>
2024-02-08 11:18:58 +0100

    feat: logout user when no activity is detected after timeout

--- src/components/auth/hoc/hoc.js
+++ src/components/auth/hoc/hoc.js
@@ -1,14 +1,31 @@
+import { listenActivity } from 'utils/events';
 import { useAuth } from 'utils/hooks/useAuth';
+import { oidcConf } from 'utils/read-env-vars';
+import { getLogoutUrl } from '../provider/component';
+
+const { noActivityTimeoutInSeconds } = oidcConf;
 
 const secure = (WrappedComponent) => {
   const Component = (props) => {
     const { oidc } = useAuth();
-    const { isUserLoggedIn, login } = oidc;
+    const { isUserLoggedIn, login, logout } = oidc;
     const { otherProps } = props;
 
     const ReturnedComponent = <WrappedComponent {...otherProps} />;
 
     if (isUserLoggedIn) {
+      (async function callee() {
+        const logoutTimeout = setTimeout(async () => {
+          console.log(
+            `No user activity detected during ${noActivityTimeoutInSeconds} seconds. Logout user.`
+          );
+          await logout({ redirectTo: 'specific url', url: getLogoutUrl() });
+        }, noActivityTimeoutInSeconds * 1000);
+
+        await listenActivity();
+        clearTimeout(logoutTimeout);
+        callee();
+      })();
       return ReturnedComponent;
     }
     login({
