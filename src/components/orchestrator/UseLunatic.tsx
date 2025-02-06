d908c12a99f5b7b18d1203c610f157d7263c0fe8
Renaud Genevois <renaud.genevois@insee.fr>
2025-02-06 09:22:49 +0100

    remove unsused changes

--- src/components/orchestrator/UseLunatic.tsx
+++ src/components/orchestrator/UseLunatic.tsx
@@ -43,7 +43,7 @@ export function UseLunatic(props: PropsWithChildren<OrchestratorProps>) {
 		disabled,
 		metadata,
 	} = props;
-	const pageReached = useRef<string>(undefined);
+
 	const [args, setArgs] = useState<Record<string, unknown>>({});
 	const [waiting, setWaiting] = useState(false);
 	const [failure, setFailure] = useState<SavingFailure>();
@@ -84,7 +84,6 @@ export function UseLunatic(props: PropsWithChildren<OrchestratorProps>) {
 			autoSuggesterLoading,
 			workersBasePath: `${window.location.origin}/workers`,
 			onChange,
-			initialPage: pageReached.current,
 		});
 	}, [
 		getReferentiel,
@@ -109,8 +108,6 @@ export function UseLunatic(props: PropsWithChildren<OrchestratorProps>) {
 		pageTag,
 		pager,
 	} = useLunatic(source, data, args);
-
-	pageReached.current = pageTag;
 
 	useEffect(() => {
 		(
